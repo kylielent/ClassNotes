c333a04a088466d19b2eb4aae2f60817cf972de4
kylielent <lentk@student.swosu.edu>
2015-10-09 09:27:33 -0500

    updated kylie1033 branch outline

--- src/Workspace.java
+++ src/Workspace.java
@@ -3,7 +3,7 @@ public class Workspace {
 	public static void main(String[] args) {
 	
 	System.out.println("Hello Class!");	
-		
+	// This is branch (  )
 	// Outline for addition problem.
 	
 	// Round 1
@@ -15,24 +15,57 @@ public class Workspace {
 	//  Tell them it was correct 
 	//	Give them points
 	//  Make the next question harder
-	//  If not correct
+	// IF not correct
+	//  Tell them it was wrong
 	//  Tell them the correct answer 
 	//  Do not give them points
 	//  Make next question easier
 	
 	// Round 2
+	// Generate Random numbers
+	// Ask the user to add these two numbers together
+	// Read in their response
+	// Check if the answer was correct
+	// IF Correct
 	//  Tell them it was correct 
 	//	Give them points
 	//  Make the next question harder
+	// IF not correct
+	//  Tell them it was wrong
+	//  Tell them the correct answer 
+	//  Do not give them points
+	//  Make next question easier
 	
 	// Round 3
+	// Generate Random numbers
+	// Ask the user to add these two numbers together
+	// Read in their response
+	// Check if the answer was correct
+	// IF Correct
 	//  Tell them it was correct 
 	//	Give them points
 	//  Make the next question harder
+	// IF not correct
+	//  Tell them it was wrong
+	//  Tell them the correct answer 
+	//  Do not give them points
+	//  Make next question easier
 	
 	
 	// Round 4
-	
+	// Generate Random numbers
+	// Ask the user to add these two numbers together
+	// Read in their response
+	// Check if the answer was correct
+	// IF Correct
+	//  Tell them it was correct 
+	//	Give them points
+	//  Make the next question harder
+	// IF not correct
+	//  Tell them it was wrong
+	//  Tell them the correct answer 
+	//  Do not give them points
+	//  Make next question easier
 	
 		
 		
