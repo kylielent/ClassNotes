6672c6f988177596fe29e9de5ca27ac180361f9d
kylielent <lentk@student.swosu.edu>
2015-10-09 09:08:33 -0500

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # ClassNotes 
+# ClassNotes2 
--- src/Workspace.java
+++ src/Workspace.java
@@ -4,7 +4,36 @@ public class Workspace {
 	
 	System.out.println("Hello Class!");	
 		
-		
+	// Outline for addition problem.
+	
+	// Round 1
+	// Generate Random numbers
+	// Ask the user to add these two numbers together
+	// Read in their response
+	// Check if the answer was correct
+	// IF Correct
+	//  Tell them it was correct 
+	//	Give them points
+	//  Make the next question harder
+	//  If not correct
+	//  Tell them the correct answer 
+	//  Do not give them points
+	//  Make next question easier
+	
+	// Round 2
+	//  Tell them it was correct 
+	//	Give them points
+	//  Make the next question harder
+	
+	// Round 3
+	//  Tell them it was correct 
+	//	Give them points
+	//  Make the next question harder
+	
+	
+	// Round 4
+	
+	
 		
 		
 		
