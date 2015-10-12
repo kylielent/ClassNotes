cfcb77609eb237f038286b5904d5502b657203cc
kylielent <lentk@student.swosu.edu>
2015-10-12 09:08:41 -0500

    Added first if else statement

--- src/Workspace.java
+++ src/Workspace.java
@@ -1,17 +1,30 @@
-
+import java.util.Scanner;
 public class Workspace {
 	public static void main(String[] args) {
-	
-	System.out.println("Hello Class!");	
-	// This is branch (  )
+	System.out.println("Hello Class!");
+	//This is my development branch kylie1033
 	// Outline for addition problem.
-	
 	// Round 1
 	// Generate Random numbers
+	int number1 = (int)(Math.random() *10);
+	System.out.println("Number1 is : " + number1 );
+	int number2 = (int)(Math.random() *10);
+	System.out.println("Number2 is : " + number2 );
+	int correctAnswer = number1 + number2;
 	// Ask the user to add these two numbers together
+	System.out.println("What is " + number1 + " + " + number2 + "?");
+	
 	// Read in their response
+	Scanner input = new Scanner(System.in);
+	int userAnswer = input.nextInt();
+	
 	// Check if the answer was correct
 	// IF Correct
+	if (userAnswer == correctAnswer){
+		System.out.println("Answer is correct. You recieve points.");
+	}else{  
+		System.out.println("Answer is incorrect. You dont get any points");
+	}
 	//  Tell them it was correct 
 	//	Give them points
 	//  Make the next question harder
@@ -51,7 +64,6 @@ public class Workspace {
 	//  Do not give them points
 	//  Make next question easier
 	
-	
 	// Round 4
 	// Generate Random numbers
 	// Ask the user to add these two numbers together
@@ -65,10 +77,6 @@ public class Workspace {
 	//  Tell them it was wrong
 	//  Tell them the correct answer 
 	//  Do not give them points
-	//  Make next question easier
-	
-		
-		
-		
+	//  Make next question easier	
 	}
 }
