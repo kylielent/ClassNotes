5c757d0bdd6f30180c41067ff098ff0c64e7aff6
kylielent <lentk@student.swosu.edu>
2015-10-12 09:53:52 -0500

    Added score and hardness to round1

--- src/Workspace.java
+++ src/Workspace.java
@@ -4,36 +4,46 @@ public class Workspace {
 	System.out.println("Hello Class!");
 	//This is my development branch kylie1033
 	// Outline for addition problem.
+	
+	int score = 5;
+	int hardnessofQuestion = 100;
 	// Round 1
 	// Generate Random numbers
-	int number1 = (int)(Math.random() *10);
+	int number1 = (int)(Math.random() * hardnessofQuestion);
 	System.out.println("Number1 is : " + number1 );
-	int number2 = (int)(Math.random() *10);
+	int number2 = (int)(Math.random() * hardnessofQuestion);
 	System.out.println("Number2 is : " + number2 );
 	int correctAnswer = number1 + number2;
+	
 	// Ask the user to add these two numbers together
 	System.out.println("What is " + number1 + " + " + number2 + "?");
 	
 	// Read in their response
 	Scanner input = new Scanner(System.in);
-	int userAnswer = input.nextInt();
+	int studentAnswer = input.nextInt();
 	
 	// Check if the answer was correct
 	// IF Correct
-	if (userAnswer == correctAnswer){
-		System.out.println("Answer is correct. You receive 5 points.");
+	if (studentAnswer == correctAnswer){
+		//Tell them it was correct and give them points
+		System.out.println("Answer is correct.");
+		score +=  hardnessofQuestion;
+		//Make next question harder
+		System.out.println("hardness is: " +  hardnessofQuestion);
+		hardnessofQuestion *= 5;
+		
+		
+		
 	}else{  
-		System.out.println("Answer is incorrect. You receive no points");
+		
+		// IF not correct
+		//  Tell them it was wrong and do not give points
+		//  Tell them the correct answer 
+		System.out.println("Answer is incorrect.");
+		System.out.println("The correct answer was: " + correctAnswer);
+		//Make next question easier
 	}
-	//  Tell them it was correct 
-	//	Give them points
-	//  Make the next question harder
 	
-	// IF not correct
-	//  Tell them it was wrong
-	//  Tell them the correct answer 
-	//  Do not give them points
-	//  Make next question easier
 	
 	// Round 2
 	// Generate Random numbers
